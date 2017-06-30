1c0b07eed2df583c697f17752af2f85f7d3a6ad9
LiJiajun <3157463498@qq.com>
2017-06-30 18:23:28 +0800

    冒泡排序
    输入5个学生的名字和分数，按照成绩高低输出他们的名字

---
+++ BubbleSort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+struct student
+{
+	char name[21];
+	int score;
+};
+int main()
+{
+	struct student a[100],t;
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		scanf("%s,%d",a[i].name,&a[i].score);
+	for(i=0;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			if(a[j].score<a[j+1].score)
+			{
+				t=a[j];
+				a[j]=a[j+1];
+				a[j+1]=a[j];
+			}
+		}
+	}
+	for (i = 1; i <= n; i++)
+	{
+		printf("%s\n",a[i].name);
+	}
+	gerchar();
+	return 0;
+}
